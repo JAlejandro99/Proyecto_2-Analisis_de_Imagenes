4e8c96d95bc320e9835d8f18dab86e304ee44e32
Jesus Dominguez <dominguezreyesjesus142014@gmail.com>
2021-05-15 20:08:13 -0500

    15 de Mayo de 2021, Reepositorio inicializado

---
+++ Inicio.py
@@ -0,0 +1,425 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+import cv2 as cv
+from PIL import Image,ImageTk
+from numpy import *
+from operaciones_histograma import *
+
+def drag_start(event):
+    global img_sel,seleccion_anterior
+    widget = event.widget
+    widget.startX = event.x
+    widget.startY = event.y
+    if seleccion_anterior!=-1:
+        imagenesLabel[seleccion_anterior].config(bd=0)
+    img_sel = imagenesLabel.index(widget)
+    #print(img_sel)
+    imagenesLabel[img_sel].config(bd=10,relief="groove")
+    seleccion_anterior = img_sel
+
+def drag_motion(event):
+    widget = event.widget
+    x = widget.winfo_x() - widget.startX + event.x
+    y = widget.winfo_y() - widget.startY + event.y
+    widget.place(x=x,y=y)
+
+def button_hover(e):
+    widget = e.widget
+    if widget==b1:
+        status_label.config(text="Ver histograma")
+    elif widget==b2:
+        status_label.config(text="Ver histograma RGB")
+    elif widget==b3:
+        status_label.config(text="Desplazar histograma a la izquierda")
+    elif widget==b4:
+        status_label.config(text="Desplazar histograma a la derecha")
+    elif widget==b5:
+        status_label.config(text="Estiramiento del histograma")
+    elif widget==b6:
+        status_label.config(text="Histograma de la imagen ecualizada")
+    elif widget==b7:
+        status_label.config(text="Estrechamiento del histograma")
+    elif widget==b8:
+        status_label.config(text="Eliminar imagen")
+
+def button_hover_leave(e):
+    status_label.config(text="")
+
+#Ventana para elgir un valor de desplazamiento
+def eligeDesp(desplazamiento):
+    
+    def guardaDato():
+        a = -1
+
+        try:
+            a = int(my_spin.get())
+            
+            #Comprueba el valor del dato ingresado
+            if(a>=0 and a<=255):
+               despWindow.destroy()
+               if(desplazamiento == False):
+                   #Desplazamiento a la izquierda
+                   img = desplazamiento_i(im[img_sel],a)
+                   agregar_img(img)
+               else:
+                    #Desplazamiento a la derecha
+                    img = desplazamiento_d(im[img_sel],a)
+                    agregar_img(img)
+                   
+            else:
+                messagebox.showerror(message="El valor debe ser un entero entre 0 y 255", 
+                             title="Valor fuera de rango")
+            
+        except:
+            messagebox.showerror(message="No se aceptan caracteres, solo números", 
+                             title="Tipo de dato incorrecto")
+
+    #Ubica la ventana en el centro
+    ancho_v = 200
+    alto_v = 150
+
+    x_v = root.winfo_screenwidth() // 2 - ancho_v // 2
+    y_v = root.winfo_screenheight() // 2 - alto_v // 2
+
+    pos = str(ancho_v) + "x" + str(alto_v) + "+" + str(x_v) + "+" + str(y_v)
+    despWindow = Toplevel(root)
+    despWindow.title("Desplazamiento")
+    despWindow.geometry(pos)
+    despWindow.resizable(ancho_v, alto_v)
+    
+    lblspin = Label(despWindow, text="Elige un valor de desplazamiento")
+    lblspin.pack(pady=10)
+    
+    #Spinbox para elegir un valor
+    my_spin = Spinbox(despWindow, from_=0, to=255,)
+    my_spin.pack(pady=10)
+    
+    #Boton para enviar
+    my_button = Button(despWindow, 
+                       text="Aceptar", 
+                       command= guardaDato)
+    my_button.pack(pady=20)
+
+#Ventana para elegir valores de estrechamiento
+def eligeEstr():
+ 
+    def guardaDatos():
+        Cmax = 0
+        Cmin = 0
+
+        try:
+            Cmin = int(my_spin.get())
+            Cmax = int(my_spin2.get())
+            
+            #Comprueba el valor de los datos ingresado
+            if((Cmax>=0 and Cmax<=255) and (Cmin>=0 and Cmin<=255)):
+                despWindow.destroy()
+                img = estrechamiento(im[img_sel],Cmin, Cmax)
+                agregar_img(img)
+                                   
+            else:
+                messagebox.showerror(message="El valor debe ser un entero entre 0 y 255", 
+                             title="Valor fuera de rango")
+            
+        except:
+            messagebox.showerror(message="No se aceptan caracteres, solo números", 
+                             title="Tipo de dato incorrecto")
+
+    #Ubica la ventana en el centro
+    ancho_v = 300
+    alto_v = 200
+
+    x_v = root.winfo_screenwidth() // 2 - ancho_v // 2
+    y_v = root.winfo_screenheight() // 2 - alto_v // 2
+
+    pos = str(ancho_v) + "x" + str(alto_v) + "+" + str(x_v) + "+" + str(y_v)
+   
+    despWindow = Toplevel(root)
+    despWindow.title("Estrechamiento")
+    despWindow.geometry(pos)
+    
+    lblspin = Label(despWindow, text="Elige los valores de compresión")
+    lblspin.pack()
+    
+    lblspin2 = Label(despWindow, text="Cmin:")
+    lblspin2.place(x=40, y=50)
+    lblspin.pack(pady=10)
+    
+    #Spinbox para elegir el valor de Cmin
+    my_spin = Spinbox(despWindow, from_=0, to=255,)
+    my_spin.place(x=50, y=20)
+    my_spin.pack(pady=10)
+    
+    lblspin2 = Label(despWindow, text="Cmax:")
+    lblspin2.place(x=40, y=90)
+    lblspin.pack()
+
+    #Spinbox para elegir el valor de Cmax
+    my_spin2 = Spinbox(despWindow, from_=0, to=255,)
+    my_spin2.place(x=50, y=70)
+    my_spin2.pack(pady=10)
+    
+    
+    #Boton para enviar
+    my_button = Button(despWindow, 
+                       text="Aceptar", 
+                       command= guardaDatos)
+    my_button.pack(pady=20)
+
+#Funcion con la que se pide una imagen al usuario
+def addImg():
+    global nomb_imagenes,im,imagenes,imagenesLabel,img_sel,seleccion_anterior
+    #Pedir nombre del archivo
+    nom_img = filedialog.askopenfilename(title="Seleccione archivo",filetypes=(("jpeg files",".jpg"),("png files",".png"),("all files",".*")))
+    nomb_imagenes.append(nom_img)
+    img = cv.imread(nom_img)
+    h = img.shape[0]
+    w = img.shape[1]
+    im.append(img)
+    img = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+    imagenes.append(ImageTk.PhotoImage(Image.fromarray(img)))
+    img_sel = len(imagenes)-1
+    imagenesLabel.append(Label(frame,image=imagenes[len(imagenes)-1],width=w,height=h,bg="gray"))
+    imagenesLabel[img_sel].place(x=100,y=100)
+    imagenesLabel[img_sel].bind("<Button-1>",drag_start)
+    imagenesLabel[img_sel].bind("<B1-Motion>",drag_motion)
+    imagenesLabel[img_sel].config(bd=10,relief="groove")
+    if seleccion_anterior!=-1:
+        imagenesLabel[seleccion_anterior].config(bd=0)
+    seleccion_anterior = img_sel
+
+def saveImg():
+    global im,nomb_imagenes
+    if nomb_imagenes[img_sel]=="nueva":
+        nom_img = filedialog.asksaveasfilename(title="Nombre del archivo a guardar",filetypes=(("jpeg files",".jpg"),("png files",".png"),("all files",".*")))
+        cv.imwrite(nom_img,im[img_sel])
+        nomb_imagenes[img_sel] = nom_img
+
+def agregar_img(img):
+    global nomb_imagenes,im,imagenes,imagenesLabel,img_sel,seleccion_anterior
+    #Pedir nombre del archivo
+    nom_img = "nueva"
+    nomb_imagenes.append(nom_img)
+    h = img.shape[0]
+    w = img.shape[1]
+    im.append(img)
+    img = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+    imagenes.append(ImageTk.PhotoImage(Image.fromarray(img)))
+    img_sel = len(imagenes)-1
+    imagenesLabel.append(Label(frame,image=imagenes[len(imagenes)-1],width=w,height=h,bg="gray"))
+    imagenesLabel[img_sel].place(x=100,y=100)
+    imagenesLabel[img_sel].bind("<Button-1>",drag_start)
+    imagenesLabel[img_sel].bind("<B1-Motion>",drag_motion)
+    imagenesLabel[img_sel].config(bd=10,relief="groove")
+    if seleccion_anterior!=-1:
+        imagenesLabel[seleccion_anterior].config(bd=0)
+    seleccion_anterior = img_sel
+
+def verHist():
+    #Si no hay imagenes seleccionadas muestra advertencia
+    if(len(im)==0):
+        messagebox.showwarning(message="Debes seleccionar una imagen", 
+                             title="Imagen no seleccionada")
+        return
+    #Histograma de la imagen original
+    print(img_sel)
+    hist = h_original(im[img_sel])
+    
+#Hacer que reciba imagenes en lugar de el nombre
+def verHistRGB():
+    #Si no hay imagenes seleccionadas muestra advertencia
+    if(len(im)==0):
+        messagebox.showwarning(message="Debes seleccionar una imagen", 
+                             title="Imagen no seleccionada")
+        return
+    #Si la imagen recibida solo tiene un canal muestra mensaje de error
+    if( histogramas_RGB(im[img_sel]) ):
+        messagebox.showerror(message="La imagen debe tener los 3 canales RGB", 
+                             title="Imagen en escala de grises")
+        
+
+def despIzqHist():
+    #Si no hay imagenes seleccionadas muestra advertencia
+    if(len(im)==0):
+        messagebox.showwarning(message="Debes seleccionar una imagen", 
+                             title="Imagen no seleccionada")
+        return
+    eligeDesp(False)
+    
+def despDerHist():
+    #Si no hay imagenes seleccionadas muestra advertencia
+    if(len(im)==0):
+        messagebox.showwarning(message="Debes seleccionar una imagen", 
+                             title="Imagen no seleccionada")
+        return
+    eligeDesp(True)
+
+def estHist():
+    #Si no hay imagenes seleccionadas muestra advertencia
+    if(len(im)==0):
+        messagebox.showwarning(message="Debes seleccionar una imagen", 
+                             title="Imagen no seleccionada")
+        return
+    hist=cv.calcHist([im[img_sel]], [0], None, [256], [0, 256])
+    img = estiramiento(hist,im[img_sel])
+    agregar_img(img)
+
+def histEcual():
+    #Si no hay imagenes seleccionadas muestra advertencia
+    if(len(im)==0):
+        messagebox.showwarning(message="Debes seleccionar una imagen", 
+                             title="Imagen no seleccionada")
+        return
+    img = ecualizacion(im[img_sel])
+    agregar_img(img)
+
+#Hacer que reciba imagenes en lugar de un nombre
+def histEstr():
+    #Si no hay imagenes seleccionadas
+    if(len(im)==0):
+        messagebox.showwarning(message="Debes seleccionar una imagen", 
+                             title="Imagen no seleccionada")
+        return
+    eligeEstr()
+
+def eliminar_img():
+    global imagenes,imagenesLabel,im,nomb_imagenes,seleccion_anterior,img_sel,frame
+    imagenesLabel[img_sel].config(bd=0)
+    #imagenesLabel[img_sel].place_forget() #Lo olvidamos
+    #print("T0")
+    #print(len(imagenes))
+    #print(len(imagenesLabel))
+    #print(len(im))
+    #print(len(nomb_imagenes))
+    imagenesLabel[img_sel].destroy() #Lo eliminamos de forma definitiva
+    del imagenes[img_sel]
+    del imagenesLabel[img_sel]
+    del im[img_sel]
+    del nomb_imagenes[img_sel]
+    #print("T1")
+    #print(len(imagenes))
+    #print(len(imagenesLabel))
+    #print(len(im))
+    #print(len(nomb_imagenes))
+    seleccion_anterior = -1
+    img_sel = -1
+
+def leer_imagen(nombre):
+    img = cv.imread(nombre)
+    alto, w, channel = img.shape
+    nuevo_alto = 80
+    escala = nuevo_alto/alto
+    img = PhotoImage(file=nombre)
+    if (escala-int(escala))!=0:
+        img = img.zoom(int(escala*10))
+        img = img.subsample(10)
+    else:
+        img = img.zoom(escala)
+    return img
+
+#Raíz
+root=Tk()
+root.title("Ventana de pruebas")
+
+#Ubica la raiz en el centro de la pantalla
+
+ancho_v = root.winfo_screenwidth() - 400
+alto_v = root.winfo_screenheight() - 200
+x_v = root.winfo_screenwidth() // 2 - ancho_v // 2
+y_v = root.winfo_screenheight() // 2 - alto_v // 2
+
+pos = str(ancho_v) + "x" + str(alto_v) + "+" + str(x_v) + "+" + str(y_v)
+root.geometry(pos)
+root.config(bd=20)
+
+w=700
+h=500
+x=w/2
+y=h/2
+imagenes = list()
+imagenesLabel = list()
+im = list()
+nomb_imagenes = list()
+seleccion_anterior = -1
+img_sel = -1
+
+Grid.rowconfigure(root,2,weight=1)
+Grid.columnconfigure(root,0,weight=1)
+Grid.columnconfigure(root,1,weight=1)
+Grid.columnconfigure(root,2,weight=1)
+Grid.columnconfigure(root,3,weight=1)
+Grid.columnconfigure(root,4,weight=1)
+Grid.columnconfigure(root,5,weight=1)
+Grid.columnconfigure(root,6,weight=1)
+Grid.columnconfigure(root,7,weight=1)
+
+bAddImg=Button(root,text="Abrir Imagen",command=lambda:addImg())
+bAddImg.grid(row=0,column=0)
+
+bSaveImg=Button(root,text="Guardar Imagen",command=lambda:saveImg())
+bSaveImg.grid(row=0,column=1)
+
+#Tenemos que descubrir qué imagen se seleccionó con anterioridad y la pasamos a la funcion ver histograma
+img1 = leer_imagen("iconos/histograma2.png")
+b1=Button(root,image=img1,width=80,command=lambda:verHist())
+b1.grid(row=1,column=0)
+b1.bind("<Enter>",button_hover)
+b1.bind("<Leave>",button_hover_leave)
+
+#Ver histograma RGB
+img2 = leer_imagen("iconos/histograma.png")
+b2=Button(root,image=img2,width=80,command=lambda:verHistRGB())
+b2.grid(row=1,column=1)
+b2.bind("<Enter>",button_hover)
+b2.bind("<Leave>",button_hover_leave)
+
+#Desplazamiento del histograma a la izquierda
+img3 = leer_imagen("iconos/izquierda.png")
+b3=Button(root,image=img3,width=80,command=lambda:despIzqHist())
+b3.grid(row=1,column=2)
+b3.bind("<Enter>",button_hover)
+b3.bind("<Leave>",button_hover_leave)
+
+#Desplazamiento del histograma a la derecha
+img4 = leer_imagen("iconos/derecha.png")
+b4=Button(root,image=img4,width=80,command=lambda:despDerHist())
+b4.grid(row=1,column=3)
+b4.bind("<Enter>",button_hover)
+b4.bind("<Leave>",button_hover_leave)
+
+#Estiramiento del histograma
+img5 = leer_imagen("iconos/estiramiento.png")
+b5=Button(root,image=img5,width=80,command=lambda:estHist())
+b5.grid(row=1,column=4)
+b5.bind("<Enter>",button_hover)
+b5.bind("<Leave>",button_hover_leave)
+
+#Histograma de la imagen ecualizada
+img6 = leer_imagen("iconos/ecualizacion.png")
+b6=Button(root,image=img6,width=80,command=lambda:histEcual())
+b6.grid(row=1,column=5)
+b6.bind("<Enter>",button_hover)
+b6.bind("<Leave>",button_hover_leave)
+
+#Estrechamiento del histograma
+img7 = leer_imagen("iconos/estrechamiento.png")
+b7=Button(root,image=img7,width=80,command=lambda:histEstr())
+b7.grid(row=1,column=6)
+b7.bind("<Enter>",button_hover)
+b7.bind("<Leave>",button_hover_leave)
+
+#Eliminar imagen
+img8 = leer_imagen("iconos/eliminar.png")
+b8=Button(root,image=img8,width=80,command=lambda:eliminar_img())
+b8.grid(row=1,column=7)
+b8.bind("<Enter>",button_hover)
+b8.bind("<Leave>",button_hover_leave)
+
+frame=Frame(root,height=h,width=w,bg="gray")
+frame.grid(row=2,column=0,columnspan=8,sticky="nsew")
+
+status_label = Label(root,text='',bd=1,relief=SUNKEN,anchor=E,font=(18),pady=10)
+status_label.grid(row=3,column=0,columnspan=8,sticky="nsew")
+
+root.mainloop()
---
+++ operaciones_histograma.py
@@ -0,0 +1,289 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2 as cv
+import copy
+import math
+
+#Histograma de imagen original
+def h_original(im):
+    
+    #Verifica si la imagen tiene 3 canales RGB
+    if(len(im.shape)==3):
+        #Si los tiene la convierte a escala de grises con un solo canal
+        im2=cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+    else:
+        #En caso de que la imagen original es de un canal se crea una copia
+        im2 = copy.copy(im)
+    
+    hist=cv.calcHist([im2], [0], None, [256], [0, 256])
+    plt.plot(hist, color='gray' )
+    plt.title("Histograma Original")
+    plt.xlabel('Intensidad de iluminacion')
+    plt.ylabel('Cantidad de pixeles')
+    plt.show()
+    
+    #cv.imshow('Original', im)
+    #cv.waitKey()
+    
+    return hist
+    
+# Función que muestra histogramas de los canales RGB
+# Recibe como parámetro una imagen
+def histogramas_RGB(img):
+    
+    #Si la imagen tiene solo un canal (está en escala de grises) mostrará error
+    if(len(img.shape)==2):
+        return -1
+    
+    # Lista con los 3 histogramas
+    histogramas = []
+    # Histograma canal azul
+    histogramas.append(cv.calcHist([img], [0], None, [256], [0, 256]))
+    # Histograma canal Verde
+    histogramas.append(cv.calcHist([img], [1], None, [256], [0, 256]))
+    #Histograma canal Azul
+    histogramas.append(cv.calcHist([img], [2], None, [256], [0, 256]))
+    
+    # Imprime los histogramas
+    colores = ["blue", "green", "red"]
+    for i in range (0,3):
+        plt.plot(histogramas[i], color=colores[i] )
+    plt.title("Histogramas Canales RGB")
+    plt.xlabel('Intensidad de iluminacion')
+    plt.ylabel('Cantidad de pixeles')
+    plt.show()
+
+def desplazamiento_d(im2,a):
+    #DESPLAZAMIENTO HACIA LA DERECHA
+    im = copy.copy(im2)
+    i=0
+    while i<im.shape[0]:
+        j=0
+        while j<im.shape[1]:
+            b,g,r=im[i,j]
+            v0=int(b)+a
+            v1=int(g)+a
+            v2=int(r)+a
+            if v0>255:
+                v0=255
+            if v1>255:
+                v1=255
+            if v2>255:
+                v2=255
+            im[i,j]=v0,v1,v2
+            j+=1
+        i+=1
+    
+    hist= cv.calcHist([im], [0], None, [256], [0, 256])
+    plt.plot(hist, color='gray' )
+    plt.title("Histograma Desplazamiento Derecha")
+    plt.xlabel('Intensidad de iluminacion')
+    plt.ylabel('Cantidad de pixeles')
+    plt.show()
+    #cv.imshow('DesDerecha', im)
+    #cv.waitKey()
+    return im
+
+def desplazamiento_i(im2,a):
+    #DESPLAZAMIENTO HACIA LA IZQUIERDA
+    im = copy.copy(im2)
+    i=0
+    while i<im.shape[0]:
+        j=0
+        while j<im.shape[1]:
+            b,g,r=im[i,j]
+            v0=int(b)-a
+            v1=int(g)-a
+            v2=int(r)-a
+            if v0<0:
+                v0=0
+            if v1<0:
+                v1=0
+            if v2<0:
+                v2=0
+            im[i,j]=v0,v1,v2
+            j+=1
+        i+=1
+    
+    hist= cv.calcHist([im], [0], None, [256], [0, 256])
+    plt.plot(hist, color='gray' )
+    plt.title("Histograma Desplazamiento Izquierda")
+    plt.xlabel('Intensidad de iluminacion')
+    plt.ylabel('Cantidad de pixeles')
+    plt.show()
+    #cv.imshow('DesIzquierda', im)
+    #cv.waitKey()
+    return im
+
+def estiramiento(hist,im2):
+    im = copy.copy(im2)
+    frec=[]
+    k=0
+    for i in range(0,hist.shape[0]):
+        for j in range(0,hist.shape[1]):
+            if hist[i,j]>0:
+                frec.insert(k,i)
+                k+=1
+    
+    minimo=frec[0]
+    maximo=frec[len(frec)-1]
+    
+    i=0
+    while i<im.shape[0]:
+        j=0
+        while j<im.shape[1]:
+            b,g,r=im[i,j]
+            v0=(int(b)-minimo)*255/(maximo-minimo)
+            v1=(int(g)-minimo)*255/(maximo-minimo)
+            v2=(int(r)-minimo)*255/(maximo-minimo)
+            im[i,j]=v0,v1,v2
+            j+=1
+        i+=1
+    
+    hist2= cv.calcHist([im], [0], None, [256], [0, 256])
+    plt.plot(hist2, color='gray' )
+    plt.title("Histograma Estiramiento")
+    plt.xlabel('Intensidad de iluminacion')
+    plt.ylabel('Cantidad de pixeles')
+    plt.show()
+    #cv.imshow('Estiramiento', im)
+    #cv.waitKey()
+    
+    return im
+
+#ECUALIZACIÓN
+def ecualizacion(im2):
+    
+    """#Verifica si la imagen tiene 3 canales RGB
+    if(len(im2.shape)==3):
+        #Si los tiene la convierte a escala de grises con un solo canal
+        im=cv.cvtColor(im2, cv.COLOR_BGR2GRAY)
+    else: 
+        #En caso de que la imagen original es de un canal se crea una copia
+        im = copy.copy(im2)"""
+    
+    pixeles=[]
+    im=copy.copy(im2)
+    total=im.shape[0]*im.shape[1]
+    frecuencia={}
+    
+    #Valor RGB de cada pixel de la imagen
+    k=0
+    i=0
+    while i<im.shape[0]:
+        j=0
+        while j<im.shape[1]:
+            b,g,r=im[i,j]
+            pixeles.insert(k,(b+g+r))
+            k+=1
+            j+=1
+        i+=1
+    
+    pixeles=sorted(pixeles)#Se ordenan los pixeles de menor a mayor
+    minimo=min(pixeles)#Se obtiene el valor mínimo de los pixeles
+    maximo=max(pixeles)#Se obtiene el valor máximo de los pixeles
+    
+    #Guarda en un diccionario la frecuencia de cada nivel RGB
+    for pixel in pixeles:
+        if pixel in frecuencia:
+            frecuencia[pixel]+=1
+        else:
+            frecuencia[pixel]=1
+    
+    #Toma solo la frecuencia de cada nivel de RGB del diccionario         
+    n_g=list(frecuencia.values())
+    
+    #Probabilidad de ocurrencia posterior del nivel de RGB
+    pp_g=[]
+    acum=0
+    
+    for i in range(len(n_g)):
+        acum=acum+n_g[i]
+        pp_g.insert(i,(acum/total))
+    
+    #Calculando la ecualización uniforme
+    f_g=[]
+    for i in range(len(pp_g)):
+        f_g.insert(i,math.floor((maximo-minimo)*(pp_g[i]+minimo)))
+    
+    #Obtiene los valores de los pixeles RGB
+    pix=list(frecuencia.keys())
+    
+    i=0
+    while i<im.shape[0]:
+        j=0
+        while j<im.shape[1]:
+            b,g,r=im[i,j]
+            if b+g+r in pix:
+                v=pix.index(b+g+r)
+                im[i,j]=f_g[v]
+            j+=1
+        i+=1
+
+    hist4=cv.calcHist([im], [0], None, [256], [0, 256])
+    plt.plot(hist4, color='gray' )
+    plt.title("Histograma Ecualización")
+    plt.xlabel('Intensidad de iluminacion')
+    plt.ylabel('Cantidad de pixeles')
+    plt.show()
+    #cv.imshow('Ecualización', im)
+    #cv.waitKey()
+    return im
+    
+# Función que realiza estrechamiento del histograma
+# Recibe como parámetros la imagen y los valores deseados de compresión
+# Devuelve una nueva imagen (matriz numpy) con los cambios realizados
+def estrechamiento(img, Cmin, Cmax):
+    
+    #Verifica si la imagen tiene 3 canales RGB
+    if(len(img.shape)==3):
+        #Si los tiene la convierte a escala de grises con un solo canal
+        nueva_img=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    else:    
+        #En caso de que la imagen original es de un canal se crea una copia
+        nueva_img = copy.copy(img)
+    
+    # Valores de la formula
+    rmin = np.amin(img)
+    rmax = np.amax(img)
+
+    # Cambio de valores a la imagen de acuerdo a la fórmula
+    for i in range(0, nueva_img.shape[0]):
+        for j in range(0, nueva_img.shape[1]):
+            nueva_img[i,j] = round(((Cmax-Cmin)/(rmax-rmin))*nueva_img[i,j] + Cmin)
+    
+    # Calcula el histograma con estrechamiento
+    nhistograma = cv.calcHist([nueva_img], [0], None, [256], [0, 256])
+    # Muestra el histograma
+    plt.plot(nhistograma, color='gray' )
+    plt.title("Histograma Estrechamiento")
+    plt.xlabel('Intensidad de iluminacion')
+    plt.ylabel('Cantidad de pixeles')
+    plt.show()
+    
+    # Muestra los cambios realizados en la imagen
+    #cv.imshow('Estrechamiento', nueva_img)
+    #cv.waitKey()
+    
+    return nueva_img
+
+def principal():
+    im=cv.imread("Imagen1.png")
+    im2=cv.imread("Imagen1.png")
+    hist=cv.calcHist([im2], [0], None, [256], [0, 256])
+    a=50
+    
+    #Histograma de la imagen original
+    hist=h_original(im)
+    #Histogramas RGB
+    histogramas_RGB(copy.copy(im2))
+    #Histograma desplazado a la derecha
+    desplazamiento_d(im,a)
+    #Histograma desplazado a la izquierda
+    desplazamiento_i(im2,a)
+    #Estiramiento del histograma
+    estiramiento(hist,im2)
+    #Histograma de la imagen ecualizada
+    ecualizacion(im)
+    # Estrechamiento de Histograma
+    estrechamiento(copy.copy(im2), 50, 150)
